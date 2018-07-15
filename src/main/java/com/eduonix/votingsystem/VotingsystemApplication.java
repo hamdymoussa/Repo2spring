53494bd2daeca828418acbbd3f0a8db91b6e7487
hamdymoussa <hamdymoussa1@gmail.com>
2018-07-15 14:32:30 -0400

    votingsystem spring boot project

---
+++ src/main/java/com/eduonix/votingsystem/VotingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.eduonix.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingsystemApplication.class, args);
+	}
+}
